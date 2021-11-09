94904ffe422b04ae2692ca7b63be5f60abc245af
mtclinton <maxtclinton@gmail.com>
2021-11-09 13:43:05 -0500

    added error handling

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mtclinton/hello
 
 go 1.17
+
+require github.com/mtclinton/greetings v0.0.0-20211109172527-f07556dfd7a8
--- hello.go
+++ hello.go
@@ -2,12 +2,26 @@ package main
 
 import (
     "fmt"
-
+    "log"
     "github.com/mtclinton/greetings"
 )
 
 func main() {
+    // Set properties of the defined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+
     // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
+    message err := greetings.Hello("Gladys")
+    // If an error was returned, print it to the console and
+    // exit the program
+    if err != nil {
+	    log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message to console
     fmt.Println(message)
 }
