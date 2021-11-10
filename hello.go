65278c8bd180470ab76e5361aca800c7a71b7476
mtclinton <maxtclinton@gmail.com>
2021-11-09 21:08:36 -0500

    sending multiple names

--- hello.go
+++ hello.go
@@ -13,15 +13,17 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
+    // A slice of names.
+    names := []string{"Max", "John", "Tim"}
 
     // Get a greeting message and print it.
-    message err := greetings.Hello("Gladys")
+    messages, err := greetings.Hellos(names)
     // If an error was returned, print it to the console and
     // exit the program
     if err != nil {
 	    log.Fatal(err)
     }
 
-    // If no error was returned, print the returned message to console
-    fmt.Println(message)
+    // If no error was returned, print the returned messages to console
+    fmt.Println(messages)
 }
